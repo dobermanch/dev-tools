f5443928b9830197b21a0eba41cfa11ecce92241
Sergii Chevkota <chevkota@live.com>
2024-06-28 23:05:26 -0700

    Tool (base64-encoder): encode string to base64 string

---
+++ src/Dev.Tools/Tools/Base64EncoderTool.cs
@@ -0,0 +1,48 @@
+﻿using Dev.Tools.Core;
+using System.Text;
+
+namespace Dev.Tools.Tools;
+
+[ToolDefinition(
+    Name = "base64-encoder",
+    Aliases = ["e64"]
+    // Keywords = [ToolKeyword.Base64, ToolKeyword.Encode, ToolKeyword.Url]
+    // Categories = [ToolCategory.Converter],
+    // ErrorCodes = [ToolError.Unknown, "TEXT_EMPTY"]
+)]
+public class Base64EncoderTool : ToolBase<Base64EncoderTool.Args, Base64EncoderTool.Result>
+{
+    protected override Result Execute(Args args)
+    {
+        if (string.IsNullOrEmpty(args.Text))
+        {
+            return Failed("TEXT_EMPTY");
+        }
+
+        byte[] bytes = Encoding.UTF8.GetBytes(args.Text);
+
+        string encoded = Convert.ToBase64String(
+                bytes,
+                args.InsertLineBreaks
+                    ? Base64FormattingOptions.InsertLineBreaks
+                    : Base64FormattingOptions.None
+            );
+
+        if (args.UrlSafe)
+        {
+            encoded = encoded
+                .Replace('+', '-')
+                .Replace('/', '_')
+                .TrimEnd('=');
+        }
+
+        return new(encoded);
+    }
+
+    public record Args(string Text, bool InsertLineBreaks = false, bool UrlSafe = false) : ToolArgs;
+
+    public record Result(string Text) : ToolResult
+    {
+        public Result() : this(string.Empty) { }
+    }
+}
---
+++ tests/Dev.Tools.Tests/Tools/Base64EncoderToolTests.cs
@@ -0,0 +1,58 @@
+﻿using Dev.Tools.Tools;
+
+namespace Dev.Tools.Tests.Tools;
+
+public class Base64EncoderToolTests
+{
+    [Theory]
+    [InlineData("Test data 123 !", "VGVzdCBkYXRhIDEyMyAh")]
+    [InlineData("Тест дата 123 !", "0KLQtdGB0YIg0LTQsNGC0LAgMTIzICE=")]
+    [InlineData(
+        "Character set: In case of textual data, the encoding scheme does not contain the character set, so you have to specify which character set was used during the encoding process.",
+        "Q2hhcmFjdGVyIHNldDogSW4gY2FzZSBvZiB0ZXh0dWFsIGRhdGEsIHRoZSBlbmNvZGluZyBzY2hlbWUgZG9lcyBub3QgY29udGFpbiB0aGUgY2hhcmFjdGVyIHNldCwgc28geW91IGhhdmUgdG8gc3BlY2lmeSB3aGljaCBjaGFyYWN0ZXIgc2V0IHdhcyB1c2VkIGR1cmluZyB0aGUgZW5jb2RpbmcgcHJvY2Vzcy4="
+    )]
+    public async Task ShouldEncodeStringToBase64_WhenStringSpecified(string text, string expectedResult)
+    {
+        var args = new Base64EncoderTool.Args(text);
+
+        var result = await new Base64EncoderTool().RunAsync(args, CancellationToken.None);
+
+        Assert.Equal(expectedResult, result.Text);
+    }
+
+    [Theory]
+    [InlineData(
+        "Character set: In case of textual data, the encoding scheme does not contain the character set, so you have to specify which character set was used during the encoding process.",
+        "Q2hhcmFjdGVyIHNldDogSW4gY2FzZSBvZiB0ZXh0dWFsIGRhdGEsIHRoZSBlbmNvZGluZyBzY2hl\r\nbWUgZG9lcyBub3QgY29udGFpbiB0aGUgY2hhcmFjdGVyIHNldCwgc28geW91IGhhdmUgdG8gc3Bl\r\nY2lmeSB3aGljaCBjaGFyYWN0ZXIgc2V0IHdhcyB1c2VkIGR1cmluZyB0aGUgZW5jb2RpbmcgcHJv\r\nY2Vzcy4="
+    )]
+    public async Task ShouldSplitEncodeStringOnNewLines_WhenInsertLineBreaksIsSet(string text, string expectedResult)
+    {
+        var args = new Base64EncoderTool.Args(text, InsertLineBreaks: true);
+
+        var result = await new Base64EncoderTool().RunAsync(args, CancellationToken.None);
+
+        Assert.Equal(expectedResult, result.Text);
+    }
+
+    [Theory]
+    [InlineData("Тест дата 123 !", "0KLQtdGB0YIg0LTQsNGC0LAgMTIzICE")]
+    public async Task ShouldEncodeToUrlSafeString_WhenUrlSafeIsSet(string text, string expectedResult)
+    {
+        var args = new Base64EncoderTool.Args(text, UrlSafe: true);
+
+        var result = await new Base64EncoderTool().RunAsync(args, CancellationToken.None);
+
+        Assert.Equal(expectedResult, result.Text);
+    }
+
+    [Fact]
+    public async Task ShouldReturnEmptyErrorCode_WhenTextIsNotProvided()
+    {
+        var args = new Base64EncoderTool.Args("");
+
+        var result = await new Base64EncoderTool().RunAsync(args, CancellationToken.None);
+
+        Assert.True(result.HasErrors);
+        Assert.Equal("TEXT_EMPTY", result.ErrorCodes[0]);
+    }
+}
