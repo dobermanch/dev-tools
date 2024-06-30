f9a51ec0be50e819a14ce328297a7460a754e7bf
Sergii Chevkota <chevkota@live.com>
2024-06-30 14:12:41 -0700

    Tool (uuid-gen): generates UUID of different version

---
+++ src/Dev.Tools/Tools/UuidGeneratorTool.cs
@@ -0,0 +1,146 @@
+﻿using Dev.Tools.Core;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Dev.Tools.Tools;
+
+[ToolDefinition(
+    Name = "uuid-gen",
+    Aliases = ["ug"]
+// Keywords = [ToolKeyword.Uuid, ToolKeyword.Guid, ToolKeyword.Generate]
+// Categories = [ToolCategory.Converter],
+// ErrorCodes = [ToolError.Unknown, "NAMESAPCE_EMPTY"]
+)]
+public sealed class UuidGeneratorTool : ToolBase<UuidGeneratorTool.Args, UuidGeneratorTool.Result>
+{
+    private readonly static Guid _max = Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF");
+
+    protected override Result Execute(Args args)
+    {
+        Guid[] guids;
+
+        switch (args.Type)
+        {
+            case UuidType.Nil:
+                guids = Generate(args, _ => Guid.Empty);
+                break;
+            case UuidType.Max:
+                guids = Generate(args, _ => _max);
+                break;
+            case UuidType.V3:
+                if (args.Namespace is null)
+                {
+                    return Failed("NAMESPACE_EMPTY");
+                }
+
+                guids = Generate(args, _ => NewUuidV3(args.Namespace.Value, args.Name ?? string.Empty));
+                break;
+            case UuidType.V4:
+                guids = Generate(args, _ => Guid.NewGuid());
+                break;
+            case UuidType.V5:
+                if (args.Namespace is null)
+                {
+                    return Failed("NAMESPACE_EMPTY");
+                }
+
+                guids = Generate(args, _ => NewUuidV5(args.Namespace.Value, args.Name ?? string.Empty));
+                break;
+            case UuidType.V7:
+                long timestamp = (args.time ?? DateTimeOffset.UtcNow).ToUnixTimeMilliseconds();
+                guids = Generate(args, _ => NewUuidV7(timestamp));
+                break;
+            default:
+                guids = [];
+                break;
+        }
+
+        return new(guids);
+    }
+
+    private static Guid[] Generate(Args args, Func<long, Guid> getGuid)
+    {
+        var guilds = new Guid[args.Count];
+        Parallel.For(0, guilds.Length, index => guilds[index] = getGuid(index));
+
+        return guilds;
+    }
+
+    private static Guid NewUuidV3(Guid namespaceUuid, string name)
+    {
+        using var md5 = MD5.Create();
+        return NewHashBasdUuid(md5, namespaceUuid, name);
+    }
+
+    private static Guid NewUuidV5(Guid namespaceUuid, string name)
+    {
+        using var sha1 = SHA1.Create();
+        return NewHashBasdUuid(sha1, namespaceUuid, name);
+    }
+
+    private static Guid NewHashBasdUuid(HashAlgorithm algorithm, Guid namespaceUuid, string name)
+    {
+        byte[] namespaceBytes = namespaceUuid.ToByteArray();
+        byte[] nameBytes = Encoding.UTF8.GetBytes(name);
+        byte[] bytes = namespaceBytes.Concat(nameBytes).ToArray();
+
+        byte[] hash = algorithm.ComputeHash(bytes);
+        hash[6] = (byte)((hash[6] & 0x0F) | 0x50); // Set version to 5 (0b0101)
+        hash[8] = (byte)((hash[8] & 0x3F) | 0x80); // Set variant to RFC 4122
+
+        return new Guid(hash[0..16]);
+    }
+
+    private static Guid NewUuidV7(long timeStampInMs)
+    {
+        var bytes = new byte[16];
+
+        // Fill time part
+        // bytes [0-5]: datetimeoffset yyyy-MM-dd hh:mm:ss fff
+        byte[] current = BitConverter.GetBytes(timeStampInMs);
+        if (BitConverter.IsLittleEndian)
+        {
+            Array.Reverse(current);
+        }
+
+        current[2..8].CopyTo(bytes, 0);
+
+        // Fill random part
+        // bytes [7-15]: random part
+        Span<byte> random = bytes.AsSpan().Slice(6);
+        RandomNumberGenerator.Fill(random);
+
+        // add mask to set guid version
+        // bytes [6]: 4 bits dedicated to guid version (version: 7)
+        // bytes [6]: 4 bits dedicated to random part
+        bytes[6] &= 0x0F;
+        bytes[6] += 0x70;
+
+        return new Guid(bytes, true);
+    }
+
+    public enum UuidType : byte
+    {
+        Nil,
+        V3,
+        V4,
+        V5,
+        V7,
+        Max
+    }
+
+    // TODO: Taking into account that each UUID type has it own set of parameters,
+    // it make sence to make separate tool for each type
+    public record Args(
+        UuidType Type, 
+        int Count = 1, 
+        Guid? Namespace = null, 
+        string? Name = null,
+        DateTimeOffset? time = null
+    ) : ToolArgs;
+
+    public record Result(IReadOnlyCollection<Guid> Guilds) : ToolResult
+    {
+        public Result() : this([]) { }
+    }
+}
---
+++ tests/Dev.Tools.Tests/Tools/UuidGeneratorToolTests.cs
@@ -0,0 +1,379 @@
+﻿using Dev.Tools.Tools;
+using FluentAssertions;
+
+namespace Dev.Tools.Tests.Tools;
+
+public class UuidGeneratorToolTests
+{
+    // -------------------------------
+    // NIL tests 
+
+    [Fact]
+    public async Task WhenTypeIsNil_ShoudGenerateEmptyUuid()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.Nil);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task WhenTypeIsNilAndCountProvided_ShoudGenerateRightAmountOfUuids()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.Nil, Count: 5);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(args.Count);
+        result.Guilds.Should().AllBeEquivalentTo(Guid.Empty);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsNilAndNameProvided_ShouldIgnoreName()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.Nil, Name: "name");
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().BeEmpty();
+    }
+
+    [Fact]
+    public async Task WhenTypeIsNilAndNamespaceProvided_ShouldIgnoreNamespace()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.Nil, Namespace: Guid.NewGuid());
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().BeEmpty();
+    }
+
+    // -------------------------------
+    // Max tests 
+
+    [Fact]
+    public async Task WhenTypeIsMax_ShoudGenerateEmptyUuid()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.Max);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().Be(Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"));
+    }
+
+    [Fact]
+    public async Task WhenTypeIsMaxAndCountProvided_ShoudGenerateRightAmountOfUuids()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.Max, Count: 5);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(args.Count);
+        result.Guilds.Should().AllBeEquivalentTo(Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"));
+    }
+
+    [Fact]
+    public async Task WhenTypeIsMaxAndNameProvided_ShouldIgnoreName()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.Max, Name: "name");
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1); 
+        result.Guilds.First().Should().Be(Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"));
+    }
+
+    [Fact]
+    public async Task WhenTypeIsMaxAndNamespaceProvided_ShouldIgnoreNamespace()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.Max, Namespace: Guid.NewGuid());
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().Be(Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"));
+    }
+
+    // -------------------------------
+    // V3 tests 
+
+    [Fact]
+    public async Task WhenTypeIsV3AndNamespaceNotProvided_ShoudReturnErrorCode()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V3);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().BeEmpty();
+        result.ErrorCodes.First().Code.Should().Be("NAMESPACE_EMPTY");
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV3AndNameIsNotProvided_ShoudGenerateGuilds()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V3, Namespace: Guid.NewGuid());
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().NotBe(args.Namespace!.Value);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV3AndNameAndNamespaceProvided_ShoudGenerateGuids()
+    {
+        var args = new UuidGeneratorTool.Args(
+            UuidGeneratorTool.UuidType.V3, 
+            Namespace: Guid.NewGuid(), 
+            Name: "dev-tools"
+        );
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().NotBe(args.Namespace!.Value);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV3AndNameAndNamespaceProvided_ShoudAlwaysGenerateTheSameGuids()
+    {
+        var args = new UuidGeneratorTool.Args(
+            UuidGeneratorTool.UuidType.V3,
+            Namespace: Guid.NewGuid(),
+            Name: "dev-tools"
+        );
+
+        var result1 = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+        var result2 = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result1.Guilds.First().Should().Be(result2.Guilds.First());
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV3AndNameAndNamespaceAndCountProvided_ShoudGenerateRightAmountOfUuids()
+    {
+        var args = new UuidGeneratorTool.Args(
+            UuidGeneratorTool.UuidType.V3,
+            Namespace: Guid.NewGuid(),
+            Name: "dev-tools",
+            Count: 5
+        );
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(args.Count);
+    }
+
+    // -------------------------------
+    // V4 tests 
+
+    [Fact]
+    public async Task WhenTypeIsV4_ShoudGenerateRandomUuid()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V4);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().NotBeEmpty();
+        result.Guilds.First().Should().NotBe(Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"));
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV4AndCountProvided_ShoudGenerateRightAmountOfUuids()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V4, Count: 5);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(args.Count);
+        result.Guilds.Should().OnlyHaveUniqueItems();
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV4AndNameProvided_ShouldIgnoreName()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V4, Name: "name");
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV4AndNamespaceProvided_ShouldIgnoreNamespace()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V4, Namespace: Guid.NewGuid());
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV4_ShouldAlwaysReturnsUniqueUuids()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V4, Namespace: Guid.NewGuid());
+
+        var result1 = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+        var result2 = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result1.Guilds.First().Should().NotBe(result2.Guilds.First());
+    }
+
+    // -------------------------------
+    // V5 tests 
+
+    [Fact]
+    public async Task WhenTypeIsV5AndNamespaceNotProvided_ShoudReturnErrorCode()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V5);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().BeEmpty();
+        result.ErrorCodes.First().Code.Should().Be("NAMESPACE_EMPTY");
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV5AndNameIsNotProvided_ShoudGenerateGuilds()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V5, Namespace: Guid.NewGuid());
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().NotBe(args.Namespace!.Value);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV5AndNameAndNamespaceProvided_ShoudGenerateGuids()
+    {
+        var args = new UuidGeneratorTool.Args(
+            UuidGeneratorTool.UuidType.V5,
+            Namespace: Guid.NewGuid(),
+            Name: "dev-tools"
+        );
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().NotBe(args.Namespace!.Value);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV5AndNameAndNamespaceProvided_ShoudAlwaysGenerateTheSameGuids()
+    {
+        var args = new UuidGeneratorTool.Args(
+            UuidGeneratorTool.UuidType.V5,
+            Namespace: Guid.NewGuid(),
+            Name: "dev-tools"
+        );
+
+        var result1 = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+        var result2 = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result1.Guilds.First().Should().Be(result2.Guilds.First());
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV5AndNameAndNamespaceAndCountProvided_ShoudGenerateRightAmountOfUuids()
+    {
+        var args = new UuidGeneratorTool.Args(
+            UuidGeneratorTool.UuidType.V5,
+            Namespace: Guid.NewGuid(),
+            Name: "dev-tools",
+            Count: 5
+        );
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(args.Count);
+    }
+
+    // -------------------------------
+    // V7 tests 
+
+    [Fact]
+    public async Task WhenTypeIsV7_ShoudGenerateUuid()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V7);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().NotBeEmpty();
+        result.Guilds.First().Should().NotBe(Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"));
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV7AndTimeProvided_ShoudAlwaysGenerateSimilarUuid()
+    {
+        var args = new UuidGeneratorTool.Args(
+            UuidGeneratorTool.UuidType.V7,
+            time: DateTimeOffset.UtcNow
+        );
+
+        var result1 = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+        var result2 = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        byte[] bytes1 = result1.Guilds.First().ToByteArray()[..5];
+        byte[] bytes2 = result2.Guilds.First().ToByteArray()[..5];
+
+        bytes1.Should().BeEquivalentTo(bytes2);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV7AndTimeAndCountProvided_ShoudAlwaysGenerateUniqueUuid()
+    {
+        var args = new UuidGeneratorTool.Args(
+            UuidGeneratorTool.UuidType.V7,
+            time: DateTimeOffset.UtcNow,
+            Count: 5
+        );
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(args.Count);
+        result.Guilds.Should().OnlyHaveUniqueItems();
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV7AndCountProvided_ShoudGenerateRightAmountOfUuids()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V7, Count: 5);
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(args.Count);
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV7AndNameProvided_ShouldIgnoreName()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V7, Name: "name");
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().NotBeEmpty();
+        result.Guilds.First().Should().NotBe(Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"));
+    }
+
+    [Fact]
+    public async Task WhenTypeIsV7AndNamespaceProvided_ShouldIgnoreNamespace()
+    {
+        var args = new UuidGeneratorTool.Args(UuidGeneratorTool.UuidType.V7, Namespace: Guid.NewGuid());
+
+        var result = await new UuidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Guilds.Should().HaveCount(1);
+        result.Guilds.First().Should().NotBeEmpty();
+        result.Guilds.First().Should().NotBe(Guid.Parse("FFFFFFFF-FFFF-FFFF-FFFF-FFFFFFFFFFFF"));
+    }
+}
