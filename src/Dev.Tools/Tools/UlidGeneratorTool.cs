06bcc236c052e3256aa6900c7d0e4d4e86581cdb
Sergii Chevkota <chevkota@live.com>
2024-09-19 22:32:52 -0700

    Tool (ulid): Generates ULID

---
+++ src/Dev.Tools/Tools/UlidGeneratorTool.cs
@@ -0,0 +1,48 @@
+﻿namespace Dev.Tools.Tools;
+
+[ToolDefinition(
+    Name = "ulid",
+    Aliases = [],
+    Keywords = [Keyword.Uuid, Keyword.Guid, Keyword.Generate, Keyword.Text, Keyword.String],
+    Categories = [Category.Crypto],
+    ErrorCodes = []
+)]
+public sealed class UlidGeneratorTool : ToolBase<UlidGeneratorTool.Args, UlidGeneratorTool.Result>
+{
+    protected override Result Execute(Args args)
+    {
+        Ulid[] guids = args.Type switch
+        {
+            UlidType.Min => Generate(args, () => Ulid.MinValue),
+            UlidType.Max => Generate(args, () => Ulid.MaxValue),
+            _ => Generate(args, () => Ulid.NewUlid()),
+        };
+
+        return new(guids);
+    }
+
+    private static Ulid[] Generate(Args args, Func<Ulid> getGuid)
+    {
+        var ulids = new Ulid[args.Count <= 0 ? 1 : args.Count];
+        Parallel.For(0, ulids.Length, index => ulids[index] = getGuid());
+
+        return ulids;
+    }
+
+    public enum UlidType : byte
+    {
+        Random,
+        Min,
+        Max
+    }
+
+    public record Args(
+        UlidType Type,
+        int Count = 1
+    ) : ToolArgs;
+
+    public record Result(IReadOnlyCollection<Ulid> Data) : ToolResult
+    {
+        public Result() : this([]) { }
+    }
+}
---
+++ tests/Dev.Tools.Tests/Tools/UlidGeneratorToolTests.cs
@@ -0,0 +1,46 @@
+﻿using Dev.Tools.Tools;
+using FluentAssertions;
+
+namespace Dev.Tools.Tests.Tools;
+
+public class UlidGeneratorToolTests
+{
+    [Theory]
+    [InlineData(1)]
+    [InlineData(5)]
+    public async Task WhenTypeIsMin_ShoudGenerateAllMinUlid(int count)
+    {
+        var args = new UlidGeneratorTool.Args(UlidGeneratorTool.UlidType.Min, count);
+
+        var result = await new UlidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Data.Should().HaveCount(count);
+        result.Data.All(it => it == Ulid.MinValue);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(5)]
+    public async Task WhenTypeIsMax_ShoudGenerateAllMaxUlid(int count)
+    {
+        var args = new UlidGeneratorTool.Args(UlidGeneratorTool.UlidType.Max, count);
+
+        var result = await new UlidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Data.Should().HaveCount(count);
+        result.Data.All(it => it == Ulid.MaxValue);
+    }
+
+    [Theory]
+    [InlineData(1)]
+    [InlineData(5)]
+    public async Task WhenTypeIsRandom_ShoudGenerateAllRandomUlid(int count)
+    {
+        var args = new UlidGeneratorTool.Args(UlidGeneratorTool.UlidType.Random, count);
+
+        var result = await new UlidGeneratorTool().RunAsync(args, CancellationToken.None);
+
+        result.Data.Should().HaveCount(count);
+        result.Data.All(it => it != Ulid.MaxValue && it != Ulid.MinValue);
+    }
+}
