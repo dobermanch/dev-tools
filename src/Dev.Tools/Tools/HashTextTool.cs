a951960dee42a7613839e9669adceb7648cb52c6
Sergii Chevkota <chevkota@live.com>
2024-09-19 22:58:26 -0700

    Tool (hash): Create hash for provided text

---
+++ src/Dev.Tools/Tools/HashTextTool.cs
@@ -0,0 +1,89 @@
+﻿using System.Security.Cryptography;
+
+namespace Dev.Tools.Tools;
+
+[ToolDefinition(
+    Name = "hash",
+    Aliases = [],
+    Keywords = [Keyword.Generate, Keyword.Text, Keyword.String],
+    Categories = [Category.Text, Category.Crypto],
+    ErrorCodes = [Error.Unknown, Error.TextEmpty]
+)]
+public class HashTextTool : ToolBase<HashTextTool.Args, HashTextTool.Result>
+{
+    protected override Result Execute(Args args)
+    {
+        if (string.IsNullOrEmpty(args.Text))
+        {
+            return Failed(Error.TextEmpty);
+        }
+
+        var bytes = Encoding.UTF8.GetBytes(args.Text);
+
+        byte[] data = args.Algorithm switch
+        {
+            HashAlgorithm.Md5 => ComputeMd5(bytes),
+            HashAlgorithm.Sha1 => ComputeSha1(bytes),
+            HashAlgorithm.Sha256 => ComputeSha256(bytes),
+            HashAlgorithm.Sha384 => ComputeSha384(bytes),
+            HashAlgorithm.Sha512 => ComputeSha512(bytes),
+        };
+
+        var result = new StringBuilder();
+        for (int i = 0; i < data.Length; i++)
+        {
+            result.Append(data[i].ToString("x2"));
+        }
+
+        return new Result(result.ToString());
+    }
+
+    private byte[] ComputeMd5(byte[] data)
+    {
+        using var hash = MD5.Create();
+        return hash.ComputeHash(data);
+    }
+
+    private byte[] ComputeSha1(byte[] data)
+    {
+        using var hash = SHA1.Create();
+        return hash.ComputeHash(data);
+    }
+
+    private byte[] ComputeSha256(byte[] data)
+    {
+        using var hash = SHA256 .Create();
+        return hash.ComputeHash(data);
+    }
+
+    private byte[] ComputeSha384(byte[] data)
+    {
+        using var hash = SHA384.Create();
+        return hash.ComputeHash(data);
+    }
+
+    private byte[] ComputeSha512(byte[] data)
+    {
+        using var hash = SHA512.Create();
+        return hash.ComputeHash(data);
+    }
+
+    public enum HashAlgorithm
+    {
+        Md5,
+        Sha1,
+        Sha256,
+        Sha384,
+        Sha512
+    }
+
+    public record Args(
+        string Text,
+        HashAlgorithm Algorithm
+    ) : ToolArgs;
+
+    public record Result(string Data) : ToolResult
+    {
+        public Result() : this(string.Empty) { }
+    }
+}
---
+++ tests/Dev.Tools.Tests/Tools/HashTextToolTests.cs
@@ -0,0 +1,43 @@
+﻿using Dev.Tools.Tools;
+using FluentAssertions;
+
+namespace Dev.Tools.Tests.Tools;
+
+public class HashTextToolTests
+{
+    [Theory]
+    [InlineData(null, HashTextTool.HashAlgorithm.Md5)]
+    [InlineData("", HashTextTool.HashAlgorithm.Md5)]
+    [InlineData(null, HashTextTool.HashAlgorithm.Sha1)]
+    [InlineData("", HashTextTool.HashAlgorithm.Sha1)]
+    [InlineData(null, HashTextTool.HashAlgorithm.Sha256)]
+    [InlineData("", HashTextTool.HashAlgorithm.Sha256)]
+    [InlineData(null, HashTextTool.HashAlgorithm.Sha384)]
+    [InlineData("", HashTextTool.HashAlgorithm.Sha384)]
+    [InlineData(null, HashTextTool.HashAlgorithm.Sha512)]
+    [InlineData("", HashTextTool.HashAlgorithm.Sha512)]
+    public async Task WhenTextIsEmpty_ShouldReturnFailure(string? text, HashTextTool.HashAlgorithm algorithm)
+    {
+        var args = new HashTextTool.Args(text, algorithm);
+
+        var result = await new HashTextTool().RunAsync(args, CancellationToken.None);
+
+        result.HasErrors.Should().BeTrue();
+        result.ErrorCodes.Should().Contain(ToolConstants.Error.TextEmpty);
+    }
+
+    [Theory]
+    [InlineData("test-text", HashTextTool.HashAlgorithm.Md5, "cf0feea200efdea7d8580c7d4ef57ced")]
+    [InlineData("test-text", HashTextTool.HashAlgorithm.Sha1, "e0f1a2df4f3630efaad6aba64dbb5798bfc30866")]
+    [InlineData("test-text", HashTextTool.HashAlgorithm.Sha256, "b18939c1891ba923a4d46ccf146612094490300d22e436bf5a5d6d46d99e4225")]
+    [InlineData("test-text", HashTextTool.HashAlgorithm.Sha384, "d923a9e758850f9a4e6ff9d4132e61dfc2949a3ef581766500af191bc579554cef3834d326b90c66c9d805691aafcc89")]
+    [InlineData("test-text", HashTextTool.HashAlgorithm.Sha512, "8c077cc8620923d614e4e5631578b7ad2b3faa28db3873eecdca2151fa3357087a9d958978bcb75be8426c7db3322c28668570edb8890f79b323eded5a8f917b")]
+    public async Task WhenTextProvided_ShouldGenerateHash(string text, HashTextTool.HashAlgorithm algorithm, string expectedHash)
+    {
+        var args = new HashTextTool.Args(text, algorithm);
+
+        var result = await new HashTextTool().RunAsync(args, CancellationToken.None);
+
+        result.Data.Should().BeEquivalentTo(expectedHash);
+    }
+}
