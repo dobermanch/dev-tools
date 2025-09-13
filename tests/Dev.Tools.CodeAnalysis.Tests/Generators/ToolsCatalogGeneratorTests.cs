36ff5dbcc39d9fbdfb92948b31b95deaf494a162
Sergii Chevkota <chevkota@live.com>
2025-09-12 22:33:55 -0700

    small fix

--- tests/Dev.Tools.CodeAnalysis.Tests/Generators/ToolsCatalogGeneratorTests.cs
+++ tests/Dev.Tools.CodeAnalysis.Tests/Generators/ToolsCatalogGeneratorTests.cs
@@ -5,7 +5,7 @@ namespace Dev.Tools.CodeAnalysis.Tests.Generators;
 
 public class ToolsCatalogGeneratorTests : GeneratorTestsBase
 {
-    [Fact]
+    [Test]
     public async Task Should_Generate_Tools_Provider()
     {
         var code = new CodeBlock
