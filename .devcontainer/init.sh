8b720453342602a6ca7c293e0f6269350a934010
Sergii Chevkota <chevkota@live.com>
2025-10-30 22:27:33 -0700

    Added .vscode to git

--- .devcontainer/init.sh
+++ .devcontainer/init.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 sudo chown -R vscode:vscode /home/vscode/.dotnet
+sudo chown -R vscode:vscode /workspaces
 
 dotnet dev-certs https
 sudo -E dotnet dev-certs https --export-path /usr/local/share/ca-certificates/dotnet-dev-cert.crt --format pem
