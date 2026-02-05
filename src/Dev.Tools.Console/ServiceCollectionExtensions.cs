bf7a61d098756b0f29bf9f063f647d84c68550ab
Sergii Chevkota <chevkota@live.com>
2026-02-04 21:05:42 -0800

    Updated CLI generator to generate commands

---
+++ src/Dev.Tools.Console/ServiceCollectionExtensions.cs
@@ -0,0 +1,23 @@
+using Dev.Tools.Console.Commands;
+using Dev.Tools.Console.Core;
+using Microsoft.Extensions.DependencyInjection;
+using Spectre.Console.Cli;
+
+namespace Dev.Tools.Console;
+
+internal static class ServiceCollectionExtensions
+{
+    extension(IServiceCollection services)
+    {
+        public IServiceCollection AddCommands(CommandApp app)
+        {
+            services.AddTransient<IToolResponseHandler, ToolResponseHandler>();
+            app.Configure(config =>
+            {
+                CommandConfigurator.ConfigureCommands(services, config);
+            });
+            
+            return services;
+        }
+    }
+}
