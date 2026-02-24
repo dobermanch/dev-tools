390165be45090076c811e341e201ab1b29aa6e4c
Sergii Chevkota <chevkota@live.com>
2026-02-23 22:01:59 -0800

    one more redirect fix

--- src/Dev.Tools.Web/Services/Search/ToolsSearchProvider.cs
+++ src/Dev.Tools.Web/Services/Search/ToolsSearchProvider.cs
@@ -39,7 +39,7 @@ internal sealed class ToolsSearchProvider(WebContext webContext) : ISearchProvid
             .GetToolDefinitions()
             .Select(it => new ToolInfo(
                 it.Name,
-                $"/tools/{it.Name}",
+                $"tools/{it.Name}",
                 webContext.Localization.GetToolTitle(it),
                 webContext.Localization.GetToolDescription(it)
             ))
